221f7e074875a5dbdb4bf5754267b2e875f2c522
wsnn2672 <gunessinan267@gmail.com>
2025-08-13 13:15:11 +0300

    suggestions

---
+++ app.js
@@ -0,0 +1,24 @@
+const images = document.querySelectorAll('.section-image');
+images.forEach(img => {
+    img.style.display = 'none';
+});
+
+const checkbox = document.getElementById('toggleImages');
+checkbox.addEventListener('change', function () {
+    const images = document.querySelectorAll('.section-image');
+    images.forEach(img => {
+        img.style.display = this.checked ? 'block' : 'none';
+    });
+});
+
+const checkbox2 = document.getElementById('toggleTurkish');
+checkbox.addEventListener('change', function () {
+    const trTexts = document.querySelectorAll('.turkish-text');
+    trTexts.forEach(txt => {
+        txt.style.display = this.checked ? 'block' : 'none';
+    });
+});
+
+window.secret = function() {
+    window.location.href = "gizli/index.html";
+}
